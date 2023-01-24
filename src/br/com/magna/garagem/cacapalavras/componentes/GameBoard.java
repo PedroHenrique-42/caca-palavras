e6a0e422eda5328cda8230c45298a0d1a23efb4a
Pedro Ferezin <phlferezin03@gmail.com>
2023-01-24 16:15:30 -0300

    update

--- src/br/com/magna/garagem/cacapalavras/componentes/GameBoard.java
+++ src/br/com/magna/garagem/cacapalavras/componentes/GameBoard.java
@@ -30,39 +30,34 @@ public class GameBoard {
 			System.out.println(boardLine);
 
 			for (Integer column = 0; column < this.gameBoard[line].length; column++) {
-				if (column == 0) {
+				if (column == 0)
 					System.out.print(" " + line + " | ");
-				}
 
 				if (line == 0 && column == 1) {
 					this.gameBoard[line][column] = gameWordsContainsBola ? 'B' : findedWordCharacter;
-				} else if (line == 0 && column == 2) {
-					this.gameBoard[line][column] = gameWordsContainsBola ? 'O' : findedWordCharacter;
-				} else if (line == 0 && column == 3) {
-					this.gameBoard[line][column] = gameWordsContainsBola ? 'L' : findedWordCharacter;
-				} else if (line == 0 && column == 4) {
-					this.gameBoard[line][column] = gameWordsContainsBola ? 'A' : findedWordCharacter;
+					this.gameBoard[line][column + 1] = gameWordsContainsBola ? 'O' : findedWordCharacter;
+					this.gameBoard[line][column + 2] = gameWordsContainsBola ? 'L' : findedWordCharacter;
+					this.gameBoard[line][column + 3] = gameWordsContainsBola ? 'A' : findedWordCharacter;
 				}
 
 				else if (line == 1 && column == 0) {
 					this.gameBoard[line][column] = gameWordsContainsCarro ? 'C' : findedWordCharacter;
-				} else if (line == 2 && column == 0) {
-					this.gameBoard[line][column] = gameWordsContainsCarro ? 'A' : findedWordCharacter;
-				} else if (line == 3 && column == 0) {
-					this.gameBoard[line][column] = gameWordsContainsCarro ? 'R' : findedWordCharacter;
-				} else if (line == 4 && column == 0) {
-					this.gameBoard[line][column] = gameWordsContainsCarro ? 'R' : findedWordCharacter;
-				} else if (line == 5 && column == 0) {
-					this.gameBoard[line][column] = gameWordsContainsCarro ? 'O' : findedWordCharacter;
+					this.gameBoard[line + 1][column] = gameWordsContainsCarro ? 'A' : findedWordCharacter;
+					this.gameBoard[line + 2][column] = gameWordsContainsCarro ? 'R' : findedWordCharacter;
+					this.gameBoard[line + 3][column] = gameWordsContainsCarro ? 'R' : findedWordCharacter;
+					this.gameBoard[line + 4][column] = gameWordsContainsCarro ? 'O' : findedWordCharacter;
 				}
 
-				else if ((line == 4 && column == 3)) {
+				else if (line == 4 && column == 3) {
 					this.gameBoard[line][column] = gameWordsContainsRua ? 'R' : findedWordCharacter;
-				} else if (line == 4 && column == 4) {
-					this.gameBoard[line][column] = gameWordsContainsRua ? 'U' : findedWordCharacter;
-				} else if (line == 4 && column == 5) {
-					this.gameBoard[line][column] = gameWordsContainsRua ? 'A' : findedWordCharacter;
-				} else {
+					this.gameBoard[line][column + 1] = gameWordsContainsRua ? 'U' : findedWordCharacter;
+					this.gameBoard[line][column + 2] = gameWordsContainsRua ? 'A' : findedWordCharacter;
+				}
+
+				else if (!(line == 0 && column == 1 || line == 0 && column == 2 || line == 0 && column == 3
+						|| line == 0 && column == 4 || line == 1 && column == 0 || line == 2 && column == 0
+						|| line == 3 && column == 0 || line == 4 && column == 0 || line == 5 && column == 0
+						|| line == 4 && column == 3 || line == 4 && column == 4 || line == 4 && column == 5)) {
 					this.gameBoard[line][column] = (char) randomNumberGenerator.nextInt(65, 90);
 				}
 
